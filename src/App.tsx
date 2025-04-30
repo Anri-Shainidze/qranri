067085a8108dd8cc26b5d0b6188643ab4c54cceb
Anri-Shainidze <anrianu2006@gmail.com>
2025-05-01 00:04:03 +0400

    gj

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function App() {
   return (
     <>
       <QRCodeSVG
+        imageSettings={{
+          src: "https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-1170x780.jpg",
+          height: 50,
+          width: 50,
+          excavate: false,
+        }}
         className="m-96"
         value={"https://tailwindcss.com/docs/installation/using-vite"}
       />
